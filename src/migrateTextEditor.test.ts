5bbf1d7bc854f13b216415da11f612e2e43ef4e2
Antoine Tissier <ati@activeviam.com>
2021-10-20 16:31:29 +0200

    UI-6657 - Improve migration script warnings (#7)

--- src/migrateTextEditor.test.ts
+++ src/migrateTextEditor.test.ts
@@ -3,7 +3,15 @@ import { legacyTextEditor } from "./__test_resources__/legacyTextEditor";
 
 describe("migrateTextEditor", () => {
   it("returns the ActiveUI5 Text Editor widget state corresponding to the given ActiveUI4 Rich Text Editor widget state", () => {
-    expect(migrateTextEditor(legacyTextEditor)).toMatchInlineSnapshot(`
+    const warn = console.warn;
+    console.warn = jest.fn();
+    try {
+      const migratedTextEditor = migrateTextEditor(legacyTextEditor);
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      expect(console.warn).toHaveBeenCalledWith(
+        "The `text-editor` widget is not part of the plugin registry in the ActiveUI 5 starter.\nMake sure to add it in your project.\n See https://activeviam.com/activeui/documentation/5.0.3/docs/tutorial/yourFirstCustomWidget#extend-activeui."
+      );
+      expect(migratedTextEditor).toMatchInlineSnapshot(`
       Object {
         "displayMode": "view",
         "name": "Text Editor",
@@ -21,5 +29,8 @@ describe("migrateTextEditor", () => {
         "widgetKey": "text-editor",
       }
     `);
+    } finally {
+      console.warn = warn;
+    }
   });
 });
