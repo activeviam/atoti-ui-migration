b64622bf1ce67fd4b17f50fc4fade28914332696
Ailie McCorkindale <amc@activeviam.com>
2022-10-25 11:58:29 +0200

    check widgets

--- src/checkWidgets.ts
+++ src/checkWidgets.ts
@@ -15,31 +15,45 @@ export interface auiWidgetFolder {
   };
 }
 
-export const collectCalculatedMeasures = (
-  widgetMdx: string,
-): { [cubeName: string]: string[] } | undefined => {
-  const parsedMdx: MdxSelect = parse(widgetMdx);
-  const calculatedMeasuresUsedByWidget = Object.keys(
-    getCalculatedMeasures(parsedMdx),
-  );
-
-  const cubeName = getCubeName(parsedMdx);
-  if (calculatedMeasuresUsedByWidget.length !== 0) {
-    return {
-      [cubeName]: calculatedMeasuresUsedByWidget,
-    };
-  }
-  return undefined;
-};
-
-export const getWidgetsMdx = (widgets: auiWidgetFolder): any[] => {
+/**
+ * Takes a folder of widgets from AUI 5.0 and returns an object containing arrays of all the calculated measure found with the cubeName as the key.
+ * @example {
+ *            cubeOne: [calculatedMeasureOne, calculatedMeasureTwo],
+ *            cubeTwo: [anotherCalculatedMeasure]
+ *           }
+ */
+export const getCalculatedMeasuresFromWidgets = (
+  widgets: auiWidgetFolder,
+): { [cubeName: string]: string[] } => {
   if (!widgets.children.content.children) {
-    return [];
+    return {};
   }
+  // Create a cube object to add each calculated measure to the corresponding cube
+  const cubes: { [cubeName: string]: string[] } = {};
   const widgetsArray = Object.values(widgets.children.content.children);
-  return widgetsArray.map((widget) => {
+
+  // For each widget, find the calculated measures
+  widgetsArray.map((widget) => {
     const mdx = JSON.parse(widget.entry.content).query.mdx;
-    const cubeName = collectCalculatedMeasures(mdx);
-    return cubeName;
+    const parsedMdx: MdxSelect = parse(mdx);
+
+    // The keys are the names of the calculated measures
+    // A widget may have more than one calculated measure
+    const calculatedMeasuresUsedByWidget = Object.keys(
+      getCalculatedMeasures(parsedMdx),
+    );
+    const cubeName = getCubeName(parsedMdx);
+
+    // If calculated measures are found, add them to the array for the corresponding cube.
+    if (calculatedMeasuresUsedByWidget.length !== 0) {
+      // If cubes[cubeName] already exists, add calculated measures to existing array, otherwise create new array.
+      // Use Set to ensure no duplicates
+      cubes[cubeName]
+        ? [...new Set(cubes[cubeName].concat(calculatedMeasuresUsedByWidget))]
+        : (cubes[cubeName] = [...new Set(calculatedMeasuresUsedByWidget)]);
+
+      // remove calculated member definition from parsedMdx
+    }
   });
+  return cubes;
 };
