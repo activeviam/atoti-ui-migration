367caa856e989625099b4e44b84d4218ecde157b
Ailie McCorkindale <amc@activeviam.com>
2022-11-02 11:35:50 +0100

    Refactor `getCalculatedMeasuresFromSavedWidgets`

---
+++ src/migrateCalculatedMeasuresToActivePivot/getCalculatedMeasuresFromSavedWidgets.test.ts
@@ -0,0 +1,23 @@
+import { widgetsWithCalculatedMeasuresFrom2Cubes } from "../__test_resources__/aui5.0LegacyTestResources/widgetsFolderWithCalculatedMeasuresFrom2Cubes";
+import { widgetsFolder } from "../__test_resources__/aui5.0LegacyTestResources/widgetsFolder";
+import { getCalculatedMeasuresFromSavedWidgets } from "./getCalculatedMeasuresFromSavedWidgets";
+
+describe("getCalculatedMeasuresFromWidgets", () => {
+  it("returns an array of the names of the calculated measures from the saved widgets folder, organized by cube, when there is one cube with calculated measures", () => {
+    expect(getCalculatedMeasuresFromSavedWidgets(widgetsFolder)).toStrictEqual({
+      "Distinct count city": "EquityDerivativesCube",
+      "EXP pnl.Forex": "EquityDerivativesCube",
+      "Log pv.SUM": "EquityDerivativesCube",
+      "activeui5 calculated measure": "EquityDerivativesCube",
+    });
+  });
+
+  it("returns an array of the names of the calculated measures from the saved widgets folder, organized by cube, when there is more than one cube with calculated measures", () => {
+    expect(
+      getCalculatedMeasuresFromSavedWidgets(widgetsWithCalculatedMeasuresFrom2Cubes),
+    ).toStrictEqual({
+      "activeui5 calculated measure": "EquityDerivativesCube",
+      "pvSum ^ 2": "EquityDerivativesCubeDist",
+    });
+  });
+});
