e91574768f59ec8cdbc3b0995c3abcef06aef6cc
Ailie McCorkindale <amc@activeviam.com>
2022-11-04 13:15:56 +0100

    migrateCalculatedMeasuresInWidgets

--- src/migrateCalculatedMeasuresToActivePivot/migrateCalculatedMeasuresInWidgets.test.ts
+++ src/migrateCalculatedMeasuresToActivePivot/migrateCalculatedMeasuresInWidgets.test.ts
@@ -1,6 +1,6 @@
-import { widgetsWithCalculatedMeasuresFrom2Cubes } from "../__test_resources__/aui5.0LegacyTestResources/widgetsFolderWithCalculatedMeasuresFrom2Cubes";
-import { widgetsFolder } from "../__test_resources__/aui5.0LegacyTestResources/widgetsFolder";
 import { migrateCalculatedMeasuresInWidgets } from "./migrateCalculatedMeasuresInWidgets";
+import { widgetsFolder } from "../__test_resources__/aui5.0LegacyTestResources/widgetsFolder";
+import { widgetsWithCalculatedMeasuresFrom2Cubes } from "../__test_resources__/aui5.0LegacyTestResources/widgetsFolderWithCalculatedMeasuresFrom2Cubes";
 import { dataModelsForTests } from "@activeviam/data-model";
 import { getUniqueCalculatedMeasureNames } from "./getUniqueCalculatedMeasureNames";
 import { calculatedMeasures } from "../__test_resources__/aui5.0LegacyTestResources/calculatedMeasuresFolder";
@@ -12,8 +12,8 @@ const namesOfCalculatedMeasurestoMigrate = getUniqueCalculatedMeasureNames(
   getCalculatedMeasureIds(calculatedMeasures),
 );
 
-describe("getCalculatedMeasuresFromWidgets", () => {
-  it("returns an array of the names of the calculated measures from the saved widgets folder, organized by cube, when there is one cube with calculated measures", () => {
+describe("migrateCalculatedMeasuresInWidgets", () => {
+  it("removes the calculated measure definitions from the MDX of saved widgets and returns the `cubeName` corresponding to the calculated measure name when there is one cube with calculated measures", () => {
     const migratedCalculatedMeasuresInWidgets =
       migrateCalculatedMeasuresInWidgets(
         widgetsFolder,
@@ -22,20 +22,17 @@ describe("getCalculatedMeasuresFromWidgets", () => {
       );
 
     // `cubeNames` is an object containing all calculated measures used in the provided widgets folder, associated to their cubeName.
-    expect(migratedCalculatedMeasuresInWidgets.cubeNames)
-      .toMatchInlineSnapshot(`
-      Object {
-        "Distinct count city": "EquityDerivativesCube",
-        "Log pv.SUM": "EquityDerivativesCube",
-        "activeui5 calculated measure": "EquityDerivativesCube",
-      }
-    `);
+    expect(migratedCalculatedMeasuresInWidgets.cubeNames).toStrictEqual({
+      "Distinct count city": "EquityDerivativesCube",
+      "Log pv.SUM": "EquityDerivativesCube",
+      "activeui5 calculated measure": "EquityDerivativesCube",
+    });
 
-    // This is definitely defined, otherwise the snapshot would be empty.
     // "WITH  Member [Measures].[Distinct count city]..." has been removed from `query.mdx`.
     expect(
-      migratedCalculatedMeasuresInWidgets.migratedWidgetsRecord.children!
-        .content.children!["854"].entry.content,
+      // This is definitely defined, otherwise the snapshot would be empty.
+      migratedCalculatedMeasuresInWidgets.migratedWidgetsRecord.children
+        ?.content.children!["854"].entry.content,
     ).toMatchInlineSnapshot(`
       Object {
         "mapping": Object {
@@ -57,9 +54,34 @@ describe("getCalculatedMeasuresFromWidgets", () => {
         "serverKey": "Ranch 6.0",
       }
     `);
+
+    // This widget contains 2 calculated measures. They are both removed from `query.mdx`.
+    expect(
+      // This is definitely defined, otherwise the snapshot would be empty.
+      migratedCalculatedMeasuresInWidgets.migratedWidgetsRecord.children
+        ?.content.children!["0fc"].entry.content,
+    ).toMatchInlineSnapshot(`
+      Object {
+        "mapping": Object {
+          "columns": Array [],
+          "measures": Array [
+            "[Measures].[Log pv.SUM]",
+            "[Measures].[Distinct count city]",
+          ],
+          "rows": Array [
+            "[Geography].[City].[City]",
+          ],
+        },
+        "query": Object {
+          "mdx": "SELECT NON EMPTY Hierarchize(Descendants({[Geography].[City].[AllMember]}, 1, SELF_AND_BEFORE)) ON ROWS, NON EMPTY {[Measures].[Log pv.SUM], [Measures].[Distinct count city]} ON COLUMNS FROM [EquityDerivativesCube] CELL PROPERTIES VALUE, FORMATTED_VALUE, BACK_COLOR, FORE_COLOR, FONT_FLAGS",
+          "updateMode": "once",
+        },
+        "serverKey": "Ranch 6.0",
+      }
+    `);
   });
 
-  it("returns an array of the names of the calculated measures from the saved widgets folder, organized by cube, when there is more than one cube with calculated measures", () => {
+  it("removes the calculated measure definitions from the MDX of saved widgets and returns the `cubeName` corresponding to the calculated measure name when there is more than one cube with calculated measures", () => {
     const migratedCalculatedMeasuresInWidgets =
       migrateCalculatedMeasuresInWidgets(
         widgetsWithCalculatedMeasuresFrom2Cubes,
@@ -67,10 +89,61 @@ describe("getCalculatedMeasuresFromWidgets", () => {
         namesOfCalculatedMeasurestoMigrate,
       );
 
+    // `cubeNames` is an object containing all calculated measures used in the provided widgets folder, associated to their cubeName.
     expect(migratedCalculatedMeasuresInWidgets.cubeNames).toStrictEqual({
       "activeui5 calculated measure": "EquityDerivativesCube",
       "pvSum ^ 2": "EquityDerivativesCubeDist",
     });
-  });
 
+    // "pvSum ^ 2" is removed from `query.mdx` of a widget using `EquityDerivativesCubeDist`.
+    expect(
+      // This is definitely defined, otherwise the snapshot would be empty.
+      migratedCalculatedMeasuresInWidgets.migratedWidgetsRecord.children
+        ?.content.children!["53a"].entry.content,
+    ).toMatchInlineSnapshot(`
+      Object {
+        "mapping": Object {
+          "columns": Array [
+            "ALL_MEASURES",
+          ],
+          "measures": Array [
+            "[Measures].[pvSum ^ 2]",
+          ],
+          "rows": Array [
+            "[CounterParty].[CounterParty].[CounterPartyGroup]",
+          ],
+        },
+        "query": Object {
+          "mdx": "SELECT NON EMPTY {[Measures].[pvSum ^ 2]} ON COLUMNS, NON EMPTY Hierarchize(Descendants({[CounterParty].[CounterParty].[AllMember]}, 1, SELF_AND_BEFORE)) ON ROWS FROM [EquityDerivativesCubeDist] CELL PROPERTIES VALUE, FORMATTED_VALUE, BACK_COLOR, FORE_COLOR, FONT_FLAGS",
+          "updateMode": "once",
+        },
+        "serverKey": "Ranch 5.11",
+      }
+    `);
+
+    // "activeui5 calculated measure" is removed from `query.mdx` of a widget using `EquityDerivativesCube`.
+    expect(
+      // This is definitely defined, otherwise the snapshot would be empty.
+      migratedCalculatedMeasuresInWidgets.migratedWidgetsRecord.children
+        ?.content.children!["ee7"].entry.content,
+    ).toMatchInlineSnapshot(`
+      Object {
+        "mapping": Object {
+          "horizontalSubplots": Array [],
+          "sliceBy": Array [
+            "[Currency].[Currency].[Currency]",
+          ],
+          "values": Array [
+            "[Measures].[activeui5 calculated measure]",
+          ],
+          "verticalSubplots": Array [],
+        },
+        "query": Object {
+          "mdx": "SELECT NON EMPTY Hierarchize(Descendants({[Currency].[Currency].[AllMember]}, 1, SELF_AND_BEFORE)) ON ROWS, NON EMPTY {[Measures].[activeui5 calculated measure]} ON COLUMNS FROM [EquityDerivativesCube] CELL PROPERTIES VALUE, FORMATTED_VALUE, BACK_COLOR, FORE_COLOR, FONT_FLAGS",
+          "updateMode": "once",
+        },
+        "serverKey": "Ranch 6.0",
+      }
+    `);
+  });
 });
--- src/migrateCalculatedMeasuresToActivePivot/migrateCalculatedMeasuresInWidgets.ts
+++ src/migrateCalculatedMeasuresToActivePivot/migrateCalculatedMeasuresInWidgets.ts
@@ -1,5 +1,4 @@
 import {
-  AWidgetState,
   ContentEntry,
   ContentRecord,
   CubeName,
@@ -9,12 +8,17 @@ import {
   MdxSelect,
   parse,
   getCube,
+  WidgetWithQueryState,
 } from "@activeviam/activeui-sdk";
 import _intersection from "lodash/intersection";
 import _mapValues from "lodash/mapValues";
 
-import { removeCalculatedMemberDefinition, stringify } from "@activeviam/mdx";
-import produce from "immer";
+import {
+  MdxString,
+  removeCalculatedMemberDefinition,
+  stringify,
+} from "@activeviam/mdx";
+import { produce } from "immer";
 
 export interface auiWidgetFolder {
   entry: ContentEntry;
@@ -25,8 +29,8 @@ export interface auiWidgetFolder {
 }
 
 /**
- * Takes as an argument the `/ui/widgets` folder on the content server.
- * Returns an object `cubeNames` whose keys are the names of any calculated measures created in ActiveUI 5.0 that are used in any saved widgets, with the corresponding `cubeName` as a value.
+ * Takes as an argument the `/ui/widgets` folder on the content server and returns `migratedWidgetsRecord` with the calculated member definitions removed from the MDX.
+ * Also returns an object `cubeNames` whose keys are the names of any calculated measures created in ActiveUI 5.0 that are used in any saved widgets, with the corresponding `cubeName` as a value.
  * @example {
       "calculated measure A": "CubeA",
       "calculated measure B": "CubeB",
@@ -35,33 +39,35 @@ export interface auiWidgetFolder {
 export const migrateCalculatedMeasuresInWidgets = (
   widgets: auiWidgetFolder,
   dataModel: DataModel,
-  // TODO pass measure names to remove/migrate
   namesOfCalculatedMeasurestoMigrate: string[],
 ): {
   cubeNames: { [measureName: string]: CubeName };
   migratedWidgetsRecord: ContentRecord;
 } => {
-  // Create an empty object where each calculated measure used in a saved widget will be added as a key with it's cubeName as a value.
+  // Create an empty object where each calculated measure used in a saved widget will be added as a key with its cubeName as a value.
   const cubeNames: { [measureName: string]: CubeName } = {};
-  //const namesOfCalculatedMeasurestoMigrate: string[] = [];
 
-  // For each widget, find the calculated measures.
   const migratedWidgetsRecord = produce(widgets, (draft) => {
     draft.children.content.children = _mapValues(
       widgets.children.content.children,
       (widgetRecord) => {
-        const widgetState = JSON.parse(widgetRecord.entry.content);
-        const mdx = widgetState.query.mdx;
-        console.log(mdx);
+        const widgetState: WidgetWithQueryState<MdxSelect, "serialized"> =
+          JSON.parse(widgetRecord.entry.content);
+        const mdx: MdxString | undefined = widgetState.query.mdx;
+
+        if (!mdx) {
+          return widgetRecord;
+        }
         const parsedMdx: MdxSelect = parse(mdx);
 
-        //The keys are the names of the calculated measures, a widget may have more than one calculated measure.
+        // Check for intersection between calculated measures to be migrated and calculated measures used in saved widgets.
         const calculatedMeasuresKeysUsedByWidgetAndThatNeedToBeMigrated =
           _intersection(
+            // The keys are the names of the calculated measures, a widget may use more than one calculated measure.
             Object.keys(getCalculatedMeasures(parsedMdx)),
             namesOfCalculatedMeasurestoMigrate,
           );
-
+        console.log(calculatedMeasuresKeysUsedByWidgetAndThatNeedToBeMigrated);
         const cubeName = getCubeName(parsedMdx);
 
         let updatedMdx: MdxSelect = parsedMdx;
@@ -85,35 +91,8 @@ export const migrateCalculatedMeasuresInWidgets = (
           draft.entry.content = updatedWidgetState;
         });
         return updatedRecord;
-
-        // TO DO - remove calculated member definitions from parsedMdx.
       },
     );
   });
   return { cubeNames, migratedWidgetsRecord };
 };
-
-// widget record
-// const widgetRecord = {
-//   entry: {
-//     content: '{"widgetKey": "pivot-table", query: {mdx: \'some MDX\'}}',
-//   },
-// };
-// const parsedRecordContent = {
-//   widgetKey: "something",
-//   query: {
-//     mdx: ["MDX STRING"],
-//   },
-// };
-// const parsedMdx = parse("MDX STRING");
-
-// const updatedMdx = someManipulation(parsedMdx);
-// const stringifiedUpdatedMdx = stringify(updatedMdx);
-// const updatedRecordContent = produce(
-//   parsedRecordContent,
-//   (draft) => (draft.query.mdx = stringifiedUpdatedMdx),
-// );
-// const updatedRecord = produce(
-//   widgetRecord,
-//   (draft) => (draft.content = JSON.stringify(updatedRecordContent)),
-// );
