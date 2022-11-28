48204df19d76f77fa7a01605bd39c4250d3e3666
Ailie McCorkindale <amc@activeviam.com>
2022-11-28 13:22:32 +0100

    lint fix

--- src/5.0_to_5.1/migrateCalculatedMeasuresToActivePivot/migrateCalculatedMeasuresInDashboards.ts
+++ src/5.0_to_5.1/migrateCalculatedMeasuresToActivePivot/migrateCalculatedMeasuresInDashboards.ts
@@ -72,7 +72,6 @@ export const migrateCalculatedMeasuresInDashboards = (
                         cubeNames[calculatedMeasureName] = cubeName;
                       },
                     );
-                    
                   }
                 });
                 draft[pageId].content = updatedWidget;
