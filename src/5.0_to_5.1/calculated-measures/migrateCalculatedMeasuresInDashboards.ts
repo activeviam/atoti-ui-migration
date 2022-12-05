700471ae240399de959d5ccc39b044ac5c510e5c
Ailie McCorkindale <amc@activeviam.com>
2022-12-05 18:15:30 +0100

    `serializeDashboardState` from 5.1

--- src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasuresInDashboards.ts
+++ src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasuresInDashboards.ts
@@ -5,9 +5,9 @@ import {
   DataModel,
   deserializeDashboardState,
   MdxSelect,
-  serializeDashboardState,
   WidgetWithQueryState,
-} from "@activeviam/activeui-sdk-5.1";
+} from "@activeviam/activeui-sdk-5.0";
+import { serializeDashboardState } from "@activeviam/activeui-sdk-5.1";
 import { migrateCalculatedMeasuresInMdx } from "./migrateCalculatedMeasuresInMdx";
 import { produce } from "immer";
 import _uniq from "lodash/uniq";
