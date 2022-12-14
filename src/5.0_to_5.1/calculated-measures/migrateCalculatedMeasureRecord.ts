5e6ae2412fd0cf9634d3eb923d238d1399c13721
Ailie McCorkindale <amc@activeviam.com>
2022-12-14 15:44:46 +0100

    Remove `produce`

--- src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasureRecord.ts
+++ src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasureRecord.ts
@@ -5,58 +5,54 @@ import {
   MdxLiteral,
   parse,
 } from "@activeviam/activeui-sdk-5.0";
-import { produce } from "immer";
 
 /**
  * Transforms the serialized definition of a calculated measure created with ActiveUI 5.0, into one that is natively supported by ActivePivot.
+ * Mutates `legacyCalculatedMeasureContent`.
  */
 export const migrateCalculatedMeasureRecord = (
   legacyCalculatedMeasureContent: ContentRecord,
   calculatedMeasureName: string,
-): ContentRecord => {
-  const migratedRecord = produce(legacyCalculatedMeasureContent, (draft) => {
-    const {
-      expression,
-      properties,
-    }: { expression: string; properties: string[] } = JSON.parse(
-      draft.entry.content,
-    );
+): void => {
+  const {
+    expression,
+    properties,
+  }: { expression: string; properties: string[] } = JSON.parse(
+    legacyCalculatedMeasureContent.entry.content,
+  );
 
-    const formatStringExpression = properties
-      ? properties
-          .find((property) => property.startsWith("FORMAT_STRING"))
-          ?.replace("FORMAT_STRING = ", "")
-      : undefined;
+  const formatStringExpression = properties
+    ? properties
+        .find((property) => property.startsWith("FORMAT_STRING"))
+        ?.replace("FORMAT_STRING = ", "")
+    : undefined;
 
-    const additionalProperties = properties
-      .filter((property) => !property.startsWith("FORMAT_STRING"))
-      .reduce((acc, property) => {
-        const parsedProperty = parse<MdxFunction>(property);
-        console.log(parsedProperty);
-        // The first argument of `parsedProperty` is an `MdxCompoundIdentifier`.
-        const firstArg = parsedProperty.arguments[0] as MdxCompoundIdentifier;
-        // The second argument of `parsedProperty` is an `MdxLiteral`.
-        const secondArg = parsedProperty.arguments[1] as MdxLiteral;
+  const additionalProperties = properties
+    .filter((property) => !property.startsWith("FORMAT_STRING"))
+    .reduce((acc, property) => {
+      // When property is parsed an `MdxFunction` with name "=" is returned.
+      const parsedProperty = parse<MdxFunction>(property);
+      // The first argument of `parsedProperty` is an `MdxCompoundIdentifier`.
+      const firstArg = parsedProperty.arguments[0] as MdxCompoundIdentifier;
+      // The second argument of `parsedProperty` is an `MdxLiteral`.
+      const secondArg = parsedProperty.arguments[1] as MdxLiteral;
 
-        const propertyKey = firstArg.identifiers[0].value;
-        const propertyValue = secondArg.value;
-        return {
-          ...acc,
-          [propertyKey]: propertyValue,
-        };
-      }, {});
+      const propertyKey = firstArg.identifiers[0].value;
+      const propertyValue = secondArg.value;
+      return {
+        ...acc,
+        [propertyKey]: propertyValue,
+      };
+    }, {});
 
-    const migratedCalculatedMeasureContent = JSON.stringify({
-      // The className comes from Active Pivot.
-      // See https://github.com/activeviam/activepivot/blob/876981bef9a65acbb228f97c53825a356de59382/pivot/core/impl/src/main/java/com/quartetfs/biz/pivot/definitions/impl/CalculatedMemberDescription.java
-      className:
-        "com.quartetfs.biz.pivot.definitions.impl.CalculatedMemberDescription",
-      additionalProperties,
-      uniqueName: `[Measures].[${calculatedMeasureName}]`,
-      expression,
-      formatStringExpression,
-    });
-    draft.entry.content = migratedCalculatedMeasureContent;
+  legacyCalculatedMeasureContent.entry.content = JSON.stringify({
+    // The className comes from Active Pivot.
+    // See https://github.com/activeviam/activepivot/blob/876981bef9a65acbb228f97c53825a356de59382/pivot/core/impl/src/main/java/com/quartetfs/biz/pivot/definitions/impl/CalculatedMemberDescription.java
+    className:
+      "com.quartetfs.biz.pivot.definitions.impl.CalculatedMemberDescription",
+    additionalProperties,
+    uniqueName: `[Measures].[${calculatedMeasureName}]`,
+    expression,
+    formatStringExpression,
   });
-  return migratedRecord;
 };
