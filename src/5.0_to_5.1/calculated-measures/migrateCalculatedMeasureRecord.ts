2e384390186d36cca770cd2f8ac2f1a4818789c0
Ailie McCorkindale <amc@activeviam.com>
2022-12-14 14:30:20 +0100

    use `isMdxCompoundIdentifier` and `isMdxLiteral`

--- src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasureRecord.ts
+++ src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasureRecord.ts
@@ -1,4 +1,9 @@
-import { ContentRecord } from "@activeviam/activeui-sdk-5.0";
+import {
+  ContentRecord,
+  MdxFunction,
+  parse,
+} from "@activeviam/activeui-sdk-5.0";
+import { isMdxCompoundIdentifier, isMdxLiteral } from "@activeviam/mdx-5.1";
 import { produce } from "immer";
 
 /**
@@ -21,13 +26,33 @@ export const migrateCalculatedMeasureRecord = (
           .find((property) => property.startsWith("FORMAT_STRING"))
           ?.replace("FORMAT_STRING = ", "")
       : undefined;
+    console.log(properties.map(parse)[0].arguments[1]);
+
+    const additionalProperties = properties
+      .filter((property) => !property.startsWith("FORMAT_STRING"))
+      .reduce((acc, property) => {
+        const parsedProperty = parse<MdxFunction>(property);
+        const firstArg = parsedProperty.arguments[0];
+        const secondArg = parsedProperty.arguments[1];
+
+        if (isMdxCompoundIdentifier(firstArg) && isMdxLiteral(secondArg)) {
+          const propertyKey = firstArg.identifiers[0].value;
+          const propertyValue = secondArg.value;
+          return {
+            ...acc,
+            [propertyKey]: propertyValue,
+          };
+        }
+
+        return acc;
+      }, {});
 
     const migratedCalculatedMeasureContent = JSON.stringify({
       // The className comes from Active Pivot.
       // See https://github.com/activeviam/activepivot/blob/876981bef9a65acbb228f97c53825a356de59382/pivot/core/impl/src/main/java/com/quartetfs/biz/pivot/definitions/impl/CalculatedMemberDescription.java
       className:
         "com.quartetfs.biz.pivot.definitions.impl.CalculatedMemberDescription",
-      additionalProperties: {},
+      additionalProperties,
       uniqueName: `[Measures].[${calculatedMeasureName}]`,
       expression,
       formatStringExpression,
