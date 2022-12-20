9312f5bda71865579705fa7499c314e6b86e5007
Ailie McCorkindale <amc@activeviam.com>
2022-12-20 10:38:02 +0100

    PR feedback for test and delete snapshot

--- src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasures.test.ts
+++ src/5.0_to_5.1/calculated-measures/migrateCalculatedMeasures.test.ts
@@ -4,26 +4,19 @@ import { contentServer } from "../__test_resources__/contentServer";
 import { uiCalculatedMeasuresFolder } from "../__test_resources__/uiCalculatedMeasuresFolder";
 import { uiDashboardsFolder } from "../__test_resources__/uiDashboardsFolder";
 import { uiWidgetsFolder } from "../__test_resources__/uiWidgetsFolder";
-import _cloneDeep from "lodash/cloneDeep";
 
 const dataModel = dataModelsForTests.sandbox;
 
-const contentServerForTests = _cloneDeep(contentServer);
-
-contentServerForTests.children!.ui.children = {
-  ...contentServerForTests.children!.ui.children,
+contentServer.children!.ui.children = {
+  ...contentServer.children!.ui.children,
   calculated_measures: uiCalculatedMeasuresFolder,
   dashboards: uiDashboardsFolder,
   widgets: uiWidgetsFolder,
 };
-migrateCalculatedMeasures(contentServerForTests, dataModel);
+migrateCalculatedMeasures(contentServer, dataModel);
 
 describe("migrateCalculatedMeasures", () => {
-  it("Mutates `contentServerForTests` by migrating calculated measures and removing calculated measure definitions from saved widgets and saved dashboards", () => {
-    expect(contentServerForTests).toMatchSnapshot();
-  });
-
-  it("Migrates the serialized definitions of all calculated measures created with ActiveUI 5.0 and used in a saved dashboard or saved widget, into ones that are natively supported by ActivePivot", () => {
+  it("migrates the serialized definitions of all calculated measures created with ActiveUI 5.0 and used in a saved dashboard or saved widget, into ones that are natively supported by ActivePivot", () => {
     // `uiCalculatedMeasuresFolder` contains 5 calculated measures.
     // "Exp gamma sum" is not used in any saved widgets or dashboards, it is not migrated.
     // "CM in 2 cubes" is used in both `EquityDerivativesCube` and `EquityDerivativesCubeDist`.
@@ -32,8 +25,8 @@ describe("migrateCalculatedMeasures", () => {
     // "testo" and "new measure*" are already present in the `pivot/entitlements/cm` folder.
     expect(
       Object.keys(
-        contentServerForTests.children!.pivot.children!.entitlements.children!
-          .cm.children!.EquityDerivativesCube.children!,
+        contentServer.children!.pivot.children!.entitlements.children!.cm
+          .children!.EquityDerivativesCube.children!,
       ),
     ).toStrictEqual([
       "[Measures].[testo]",
@@ -46,16 +39,58 @@ describe("migrateCalculatedMeasures", () => {
 
     expect(
       Object.keys(
-        contentServerForTests.children!.pivot.children!.entitlements.children!
-          .cm.children!.EquityDerivativesCubeDist.children!,
+        contentServer.children!.pivot.children!.entitlements.children!.cm
+          .children!.EquityDerivativesCubeDist.children!,
       ),
     ).toStrictEqual(["[Measures].[CM in 2 cubes]"]);
+  });
+
+  it("changes the content of a calculated measure to the format supported by ActivePivot", () => {
+    // The `content` property has been updated to contain the properties `className`, `additionalProperties`, `uniqueName`, `expression` and `formatStringExpression`.
+    expect(
+      contentServer.children!.pivot.children!.entitlements.children!.cm
+        .children!.EquityDerivativesCube.children![
+        "[Measures].[Distinct count city]"
+      ],
+    ).toMatchInlineSnapshot(`
+      Object {
+        "entry": Object {
+          "canRead": true,
+          "canWrite": true,
+          "content": "{\\"className\\":\\"com.quartetfs.biz.pivot.definitions.impl.CalculatedMemberDescription\\",\\"additionalProperties\\":{},\\"uniqueName\\":\\"[Measures].[Distinct count city]\\",\\"expression\\":\\"Count(Descendants([Geography].[City].CurrentMember, [Geography].[City].[City]), EXCLUDEEMPTY)\\",\\"formatStringExpression\\":\\"\\\\\\"#,###.##\\\\\\"\\"}",
+          "isDirectory": false,
+          "lastEditor": "admin",
+          "owners": Array [
+            "admin",
+          ],
+          "readers": Array [
+            "admin",
+          ],
+          "timestamp": 1666091498549,
+        },
+      }
+    `);
+  });
+
+  it("adds a calculated measure which can target several cubes to each of the targeted cubes in `pivot/entitlements/cm`", () => {
+    // "CM in 2 cubes" is added to `pivot/entitlements/cm` for both `EquityDerivativesCube` and `EquityDerivativesCubeDist`.
+    expect(
+      contentServer.children!.pivot.children!.entitlements.children!.cm
+        .children!.EquityDerivativesCube.children![
+        "[Measures].[CM in 2 cubes]"
+      ],
+    ).toStrictEqual(
+      contentServer.children!.pivot.children!.entitlements.children!.cm
+        .children!.EquityDerivativesCubeDist.children![
+        "[Measures].[CM in 2 cubes]"
+      ],
+    );
   });
 
   it("removes definitions of calculated measures saved in `ui/children/calculated_measures` from saved dashboards", () => {
     // Saved dashboard "b3e" contains calculated measure "Distinct count city".
     expect(
-      contentServerForTests.children!.ui.children!.dashboards.children!.content
+      contentServer.children!.ui.children!.dashboards.children!.content
         .children!["b3e"].entry.content,
     ).not.toContain("WITH  Member [Measures]");
   });
@@ -63,7 +98,7 @@ describe("migrateCalculatedMeasures", () => {
   it("does not remove definitions of calculated measures from saved dashboards if they are not saved in `ui/children/calculated_measures`", () => {
     // Saved dashboard "a9e" contains a calculated measure that is not saved in `ui/children/calculated_measures`.
     expect(
-      contentServerForTests.children!.ui.children!.dashboards.children!.content
+      contentServer.children!.ui.children!.dashboards.children!.content
         .children!["a9e"].entry.content,
     ).toContain("WITH  Member [Measures].[Log City]");
   });
@@ -71,22 +106,24 @@ describe("migrateCalculatedMeasures", () => {
   it("removes definitions of calculated measures saved in `ui/children/calculated_measures` from saved widgets", () => {
     // Saved widget "854" contains calculated measure "Distinct count city".
     expect(
-      contentServerForTests.children!.ui.children!.widgets.children!.content
-        .children!["854"].entry.content,
+      contentServer.children!.ui.children!.widgets.children!.content.children![
+        "854"
+      ].entry.content,
     ).not.toContain("WITH  Member [Measures]");
   });
 
   it("does not remove definitions of calculated measures from saved widgets if they are not saved in `ui/children/calculated_measures`", () => {
     // Saved dashboard "a9e" contains a calculated measure that is not saved in `ui/children/calculated_measures`.
     expect(
-      contentServerForTests.children!.ui.children!.widgets.children!.content
-        .children!["ee7"].entry.content,
+      contentServer.children!.ui.children!.widgets.children!.content.children![
+        "ee7"
+      ].entry.content,
     ).toContain("WITH  Member [Measures].[activeui5 calculated measure]");
   });
 
   it("deletes the `ui/calculated_measures` folder", () => {
     expect(
-      contentServerForTests.children!.ui.children!.calculated_measures,
+      contentServer.children!.ui.children!.calculated_measures,
     ).toBeUndefined();
   });
 });
