cd443dcebf803d3314e5296e04f65fe3b04c12d8
Antoine Tissier <ati@activeviam.com>
2022-12-20 11:09:19 +0100

    rest of feedback

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -153,6 +153,7 @@ yargs
       // Handle the special case of 4.3 to 5.0 separately, as:
       // - the legacy content server structure is completely different
       // - the servers urls are needed
+      // - it is the only migration step with async logic
       if (fromVersion === "4.3") {
         await migrate_43_to_50(contentServer, {
           errorReport,
