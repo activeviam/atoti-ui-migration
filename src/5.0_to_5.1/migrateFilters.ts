658c00a62ef1acb56b92216520d3e7fe2f5c7618
Antoine Tissier <ati@activeviam.com>
2023-01-02 17:33:53 +0100

    add comment

--- src/5.0_to_5.1/migrateFilters.ts
+++ src/5.0_to_5.1/migrateFilters.ts
@@ -1,7 +1,8 @@
 import type { Mdx } from "@activeviam/activeui-sdk-5.0";
 
 /**
- * Wraps each filter in `filters` in an object.
+ * Wraps each filter in `filters` under an `mdx` property in an object.
+ * Indeed, in 5.1 filters can have an extra `isPinned` property.
  * Mutates `filters`.
  */
 export function migrateFilters(filters?: Mdx[]): void {
