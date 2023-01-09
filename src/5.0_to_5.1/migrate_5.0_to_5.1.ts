ea6df4ca9c64af580720bee3a2dfa1fe476668a0
Antoine Tissier <ati@activeviam.com>
2023-01-09 22:54:19 +0100

    Revert "explicit generics"
    This reverts commit e5c70b4f648ebf1a6e8eb4737d6cf382dbf2cd6d.

--- src/5.0_to_5.1/migrate_5.0_to_5.1.ts
+++ src/5.0_to_5.1/migrate_5.0_to_5.1.ts
@@ -2,16 +2,11 @@ import {
   deserializeWidgetState,
   deserializeDashboardState,
   parse,
-  DashboardState as DashboardState50,
-  AWidgetState as WidgetState50,
-  MdxExpression,
 } from "@activeviam/activeui-sdk-5.0";
 import {
   serializeWidgetState,
   serializeDashboardState,
   stringify,
-  DashboardState as DashboardState51,
-  AWidgetState as WidgetState51,
 } from "@activeviam/activeui-sdk-5.1";
 import { MigrationFunction } from "../migration.types";
 import { migrateCalculatedMeasures } from "./calculated-measures/migrateCalculatedMeasures";
@@ -19,20 +14,7 @@ import { migrateDashboard } from "./migrateDashboard";
 import { migrateSavedFilter } from "./migrateSavedFilter";
 import { migrateWidget } from "./migrateWidget";
 
-export const migrate_50_to_51: MigrationFunction<
-  DashboardState50<"serialized">,
-  DashboardState50<"deserialized">,
-  DashboardState51<"deserialized">,
-  DashboardState51<"serialized">,
-  WidgetState50<"serialized">,
-  WidgetState50<"deserialized">,
-  WidgetState51<"deserialized">,
-  WidgetState51<"serialized">,
-  { mdx: string },
-  { mdx: MdxExpression },
-  { mdx: MdxExpression },
-  { mdx: string }
-> = (
+export const migrate_50_to_51: MigrationFunction = (
   contentServer,
   {
     migrateDashboards,
