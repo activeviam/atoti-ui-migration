fcfa3605eeb68610804fe5bf5a78f3c4afbc1ae2
Antoine Tissier <ati@activeviam.com>
2023-01-13 16:41:21 +0100

    UI-7645 - Refactor types for extra migration callback args

--- src/5.0_to_5.1/migrateWidget.ts
+++ src/5.0_to_5.1/migrateWidget.ts
@@ -1,24 +1,25 @@
 import { AWidgetState as AWidgetState50 } from "@activeviam/activeui-sdk-5.0";
-import { DataModel } from "@activeviam/activeui-sdk-5.1";
+import { AWidgetState as AWidgetState51 } from "@activeviam/activeui-sdk-5.1";
+import { MigrateWidgetCallback } from "../migration.types";
 import { migrateCalculatedMeasuresInWidget } from "./calculated-measures/migrateCalculatedMeasuresInWidget";
 import { migrateContextValues } from "./migrateContextValues";
 import { migrateFilters } from "./migrateFilters";
 
 /**
  * Mutates a 5.0 `widgetState` into one usable in 5.1.
+ * Also mutates `measureToCubeMapping`.
  */
-export const migrateWidget = (
-  widgetState: AWidgetState50,
+export const migrateWidget: MigrateWidgetCallback<
+  AWidgetState50,
+  AWidgetState51,
   {
-    dataModels,
-    namesOfCalculatedMeasuresToMigrate,
-    measureToCubeMapping,
-  }: {
-    dataModels: { [serverKey: string]: DataModel };
     namesOfCalculatedMeasuresToMigrate: string[];
     measureToCubeMapping: { [measureName: string]: string[] };
-  },
-): void => {
+  }
+> = (
+  widgetState,
+  { dataModels, namesOfCalculatedMeasuresToMigrate, measureToCubeMapping },
+) => {
   migrateCalculatedMeasuresInWidget(widgetState, {
     dataModels,
     namesOfCalculatedMeasuresToMigrate,
