3dcebae503d1dd2fa56486e1a8aac6206f3d62f5
Antoine Tissier <ati@activeviam.com>
2023-01-20 18:47:20 +0100

    undefined maxWorkers is not accepted

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,7 @@ const esmPackages = ["@activeviam/*", "lodash-es", "monaco-editor"];
 
 // Note that in addition to the config below, there are several mocked modules under the __mocks__ folder.
 // They correspond to modules that are UI/browser-related and are transitively pulled by @activeviam dependencies, but are not necessary for `activeui-migration` to work in a Node environment.
-module.exports = {
-  // CircleCI issues: https://discuss.circleci.com/t/jest-out-of-memory-error/32263
-  maxWorkers: isCI ? "50%" : undefined,
+const config = {
   // TODO manually mock globals such as window and use the default "node" test environment instead.
   testEnvironment: "jsdom",
   moduleNameMapper: {
@@ -24,3 +22,10 @@ module.exports = {
     )}).+\\.(${extensions.join("|")})$`,
   ],
 };
+
+const ciConfig = {
+  // CircleCI issues: https://discuss.circleci.com/t/jest-out-of-memory-error/32263
+  maxWorkers: 2,
+};
+
+module.exports = isCI ? { ...config, ...ciConfig } : config;
