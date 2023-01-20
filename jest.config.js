898ae626620d4b83867392736628b2c2a6af25ca
Antoine Tissier <ati@activeviam.com>
2023-01-20 18:44:07 +0100

    final touch

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const pkgDir = require("pkg-dir");
-
-const root = pkgDir.sync();
-
-if (!root) {
-  throw new Error("Could not find root.");
-}
+const isCI = require("is-ci");
 
 const extensions = ["js", "jsx", "ts", "tsx", "mjs", "json"];
 const esmPackages = ["@activeviam/*", "lodash-es", "monaco-editor"];
@@ -13,6 +7,8 @@ const esmPackages = ["@activeviam/*", "lodash-es", "monaco-editor"];
 // Note that in addition to the config below, there are several mocked modules under the __mocks__ folder.
 // They correspond to modules that are UI/browser-related and are transitively pulled by @activeviam dependencies, but are not necessary for `activeui-migration` to work in a Node environment.
 module.exports = {
+  // CircleCI issues: https://discuss.circleci.com/t/jest-out-of-memory-error/32263
+  maxWorkers: isCI ? "50%" : undefined,
   // TODO manually mock globals such as window and use the default "node" test environment instead.
   testEnvironment: "jsdom",
   moduleNameMapper: {
