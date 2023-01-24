67ba4213a5328296a67436c6880e5be5ae8d3a2c
Antoine Tissier <ati@activeviam.com>
2023-01-24 22:23:23 +0100

    adapt existing test

--- src/5.0_to_5.1/migrateFilters.test.ts
+++ src/5.0_to_5.1/migrateFilters.test.ts
@@ -1,17 +1,40 @@
-import { parse, stringify } from "@activeviam/activeui-sdk-5.0";
+import { parse } from "@activeviam/activeui-sdk-5.0";
 import { sandboxDataModel } from "@activeviam/data-model-5.1/dist/__test_resources__";
 import { migrateFilters } from "./migrateFilters";
 
 const dataModels = { sandbox: sandboxDataModel };
 
 describe("migrateFilters", () => {
-  it("wraps the MDX representing each filter", () => {
-    const euroAsString = "[Currency].[Currency].[ALL].[AllMember].[EUR]";
-    const euro = parse(euroAsString);
-    const filters = [euro];
+  it("transforms each MdxExpression into a Filter", () => {
+    const euro = "[Currency].[Currency].[ALL].[AllMember].[EUR]";
+    const berlin = "[Geography].[City].[ALL].[AllMember].[Berlin]";
+    const filters = [euro, berlin].map(parse);
     migrateFilters(filters, { dataModels });
-    expect(filters).toHaveLength(1);
-    // @ts-expect-error The point of the migration is to mutate the filters from `mdx` to `{mdx}`.
-    expect(stringify(filters[0].mdx)).toBe(euroAsString);
+    expect(filters).toMatchInlineSnapshot(`
+      [
+        {
+          "dimensionName": "Currency",
+          "hierarchyName": "Currency",
+          "members": [
+            [
+              "AllMember",
+              "EUR",
+            ],
+          ],
+          "type": "members",
+        },
+        {
+          "dimensionName": "Geography",
+          "hierarchyName": "City",
+          "members": [
+            [
+              "AllMember",
+              "Berlin",
+            ],
+          ],
+          "type": "members",
+        },
+      ]
+    `);
   });
 });
