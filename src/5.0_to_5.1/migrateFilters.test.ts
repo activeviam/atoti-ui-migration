04c345eae51731604a4d3e919cae897e562f22b7
Antoine Tissier <ati@activeviam.com>
2023-01-24 13:06:37 +0100

    fix build

--- src/5.0_to_5.1/migrateFilters.test.ts
+++ src/5.0_to_5.1/migrateFilters.test.ts
@@ -1,12 +1,15 @@
 import { parse, stringify } from "@activeviam/activeui-sdk-5.0";
+import { sandboxDataModel } from "@activeviam/data-model-5.1/dist/__test_resources__";
 import { migrateFilters } from "./migrateFilters";
+
+const dataModels = { sandbox: sandboxDataModel };
 
 describe("migrateFilters", () => {
   it("wraps the MDX representing each filter", () => {
     const euroAsString = "[Currency].[Currency].[ALL].[AllMember].[EUR]";
     const euro = parse(euroAsString);
     const filters = [euro];
-    migrateFilters(filters);
+    migrateFilters(filters, { dataModels });
     expect(filters).toHaveLength(1);
     // @ts-expect-error The point of the migration is to mutate the filters from `mdx` to `{mdx}`.
     expect(stringify(filters[0].mdx)).toBe(euroAsString);
