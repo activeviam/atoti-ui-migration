ba62ede511addbcd0b4899ebe73d2ab67cdf7629
Antoine Tissier <ati@activeviam.com>
2023-01-26 10:59:29 +0100

    Update src/5.0_to_5.1/createFilterWithFirstMatchingCube.ts
    Co-authored-by: Elias Tounzal <eto@activeviam.com>

--- src/5.0_to_5.1/createFilterWithFirstMatchingCube.ts
+++ src/5.0_to_5.1/createFilterWithFirstMatchingCube.ts
@@ -7,7 +7,7 @@ import {
 } from "@activeviam/activeui-sdk-5.1";
 
 /**
- * Returns a {@link Filter} based on `mdx`, with the first cube containing the hierarchies expressed in the MDX expression.
+ * Returns a {@link Filter} based on `mdx`, with the first cube containing the hierarchies expressed in `mdx`.
  */
 export function createFilterWithFirstMatchingCube(
   mdx: MdxExpression,
