7c5b1c84af70f19fa4046fd67e31c2289cfbda41
Maïlys Desvignes <62394323+mailys-ds@users.noreply.github.com>
2023-02-01 11:10:47 +0100

    UI-8176 - Make property `version` of `WidgetMetaData` mandatory (#82)
    * WIP
    
    * WIP
    
    * WIP
    
    * Clean function and add test
    
    * Add TS ! to prevent build error
    
    * Fix part of build issue
    
    * Other attempt
    
    * lint
    
    ---------
    
    Co-authored-by: Elias <eto@activeviam.com>

---
+++ src/5.0_to_5.1/migrateSavedWidgetsMetaData.ts
@@ -0,0 +1,37 @@
+import { WidgetMetaData } from "@activeviam/activeui-sdk-5.1";
+import { ContentRecord } from "@activeviam/activeui-sdk-5.0";
+import { _getFilesAncestry } from "../_getFilesAncestry";
+import { _getMetaData } from "../_getMetaData";
+
+/**
+ * Ensures all the saved widgets metadata have a `version` attribute, set to 1 by default.
+ * Mutates `contentServer`.
+ */
+export const migrateSavedWidgetsMetaData = (
+  contentServer: ContentRecord,
+): void => {
+  const { content: widgetContent, structure: widgetStructure } =
+    contentServer.children?.ui.children?.widgets.children ?? {};
+
+  for (const fileId in widgetContent.children) {
+    const filesAncestry = _getFilesAncestry(widgetStructure);
+    const pathToParentFolder = filesAncestry[fileId].map(({ id }) => id);
+    const widgetMetaData = _getMetaData<WidgetMetaData>(
+      widgetStructure,
+      pathToParentFolder,
+      fileId,
+    );
+
+    if (widgetMetaData.version === undefined) {
+      widgetMetaData.version = 1;
+
+      const metadataRecord = [
+        ...pathToParentFolder,
+        fileId,
+        `${fileId}_metadata`,
+      ].reduce((acc, id) => acc.children![id], widgetStructure);
+
+      metadataRecord.entry.content = JSON.stringify(widgetMetaData);
+    }
+  }
+};
