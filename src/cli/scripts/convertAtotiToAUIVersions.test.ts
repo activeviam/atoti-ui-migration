18f6cf079e9d7861c28d0c232a1e4718780623a2
Maïlys Desvignes <62394323+mailys-ds@users.noreply.github.com>
2023-06-01 16:13:45 +0200

    Update src/cli/scripts/convertAtotiToAUIVersions.test.ts
    Co-authored-by: Benjamin Amelot <benjamin.amelot@gmail.com>

--- src/cli/scripts/convertAtotiToAUIVersions.test.ts
+++ src/cli/scripts/convertAtotiToAUIVersions.test.ts
@@ -5,7 +5,7 @@ describe("validateVersions", () => {
     const versions = convertVersions({ fromVersion: "4.3", toVersion: "5.1" });
     expect(versions).toEqual({ fromVersion: "4.3", toVersion: "5.1" });
   });
-  it("converts atotiVersion to AtotiUI versions", () => {
+  it("converts atotiVersion to Atoti UI versions", () => {
     const versions = convertVersions({ fromVersion: "0.7", toVersion: "0.8" });
     expect(versions).toEqual({ fromVersion: "5.0", toVersion: "5.1" });
   });
