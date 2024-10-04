add3b2e12282339096e2999d659e17bfad241843
Nestor Zepeda <NestorDZepeda@gmail.com>
2024-10-04 11:34:27 -0400

    Update the tests for the new function

--- src/4.3_to_5.0/_addDefaultMeasureIfNoneIsExplicitlyExpressed.test.ts
+++ src/4.3_to_5.0/_addDefaultMeasureIfNoneIsExplicitlyExpressed.test.ts
@@ -5,23 +5,57 @@ import { dataModelsForTests } from "@activeviam/data-model-5.0";
 const cube = dataModelsForTests.sandbox.catalogs[0].cubes[0];
 
 describe("_addDefaultMeasureIfNoneIsExplicitlyExpressed", () => {
-  it("adds the default measure on columns when no measure is expressed on axes, but at least one hierarchy is", () => {
+  it("adds the default measure on columns when no measure is expressed on axes, but at least one on the columns axis hierarchy is", () => {
     const mdx: MdxSelect = parse(`
         SELECT
-         [Currency].[Currency].Members ON ROWS
+         [Currency].[Currency].Members ON COLUMNS
          FROM [EquityDerivativesCube]`);
     expect(
       stringify(_addDefaultMeasureIfNoneIsExplicitlyExpressed(mdx, { cube }), {
         indent: true,
       }),
     ).toMatchInlineSnapshot(`
-            "SELECT
-              [Currency].[Currency].Members ON ROWS,
-              NON EMPTY {
-                [Measures].[contributors.COUNT]
-              } ON COLUMNS
-              FROM [EquityDerivativesCube]"
-          `);
+      "SELECT
+        Crossjoin(
+          [Currency].[Currency].Members,
+          {
+            [Measures].[contributors.COUNT]
+          }
+        ) ON COLUMNS
+        FROM [EquityDerivativesCube]"
+    `);
+  });
+
+  it("does not add the default measure on columns when there are no measures expressed and the columns axis is empty", () => {
+    const mdx: MdxSelect = parse(`
+      SELECT
+        [Currency].[Currency].Members ON ROWS
+        FROM [EquityDerivativesCube]`);
+
+    expect(
+      stringify(_addDefaultMeasureIfNoneIsExplicitlyExpressed(mdx, { cube }), {
+        indent: true,
+      }),
+    ).toMatchInlineSnapshot(`
+      "SELECT
+        [Currency].[Currency].Members ON ROWS
+        FROM [EquityDerivativesCube]"
+    `);
+  });
+
+  it("returns the given mdx if there are no axes expressed", () => {
+    const mdx: MdxSelect = parse(`
+      SELECT
+        FROM [EquityDerivativesCube]`);
+
+    expect(
+      stringify(_addDefaultMeasureIfNoneIsExplicitlyExpressed(mdx, { cube }), {
+        indent: true,
+      }),
+    ).toMatchInlineSnapshot(`
+      "SELECT
+        FROM [EquityDerivativesCube]"
+    `);
   });
 
   it("returns the given MDX if a measure is already expressed", () => {
